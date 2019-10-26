c79cf34203cd2fc8f7183ea5a155059276cd5dc9
YuriDuque <yurithielmann83@gmail.com>
2019-10-26 00:29:57 -0300

    lesson 38 - components - implementing an approval card

--- components/src/index.js
+++ components/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from 'faker'
 
-import CommentDetail from "./CommentDetail.js"
+import CommentDetail from "./CommentDetail.js";
+import ApprovalCard from "./ApprovalCard.js";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
       <CommentDetail author="Alex" timeAgo="Today at 2:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
       <CommentDetail author="Jane" timeAgo="Yesterday at 5:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
-    </div>
+    </div >
   );
 };
 
