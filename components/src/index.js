ea3107afa18d8384589f067d6ed7326b04d222ce
YuriDuque <yurithielmann83@gmail.com>
2019-10-26 00:18:55 -0300

    lesson 36 - components - passing props - solutions

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from 'faker'
 
 import CommentDetail from "./CommentDetail.js"
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:45PM" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:45PM" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 2:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:45PM" content={faker.lorem.lines(1)} avatar={faker.image.avatar()} />
     </div>
   );
 };
