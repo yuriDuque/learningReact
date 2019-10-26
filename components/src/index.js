825d82823f358b8b521ccfa6254963d4634d787b
YuriDuque <yurithielmann83@gmail.com>
2019-10-25 23:18:09 -0300

    lesson 28 - components - native component approach

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+        </a>
+          <div className="metadata">
+            <span className="date">Today ar 6:00 PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
