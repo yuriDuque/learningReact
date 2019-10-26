23928b35c793d256cabad664a58769d4c8e4b1e0
YuriDuque <yurithielmann83@gmail.com>
2019-10-25 23:46:20 -0300

    lesson 32 - components - component nesting

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker'
+
+import CommentDetail from "./CommentDetail.js"
 
 const App = () => {
   return (
     <div className="ui container comments">
-
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
