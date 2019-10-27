d4dc91127d53cd46eb1e4f6597bb91d7da68b72a
YuriDuque <yurithielmann83@gmail.com>
2019-10-27 14:24:25 -0300

    lesson 48 - seasons - geting a user physical location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return (
     <div className="ui container comments">
     </div >
