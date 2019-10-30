035a6f6fe9b94457ba38d00200fa3b75031d3d1d
YuriDuque <yurithielmann83@gmail.com>
2019-10-29 22:52:31 -0300

    lesson 63 - seasons - determining season

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const SeasonDisplay = (props) => {
-  return <div>Latitute: {props.lat}</div>
+class SeasonDisplay extends React.Component {
+
+  state = {
+    season: "",
+    month: ""
+  }
+
+  render() {
+    return <div>Latitute: {this.state.season}</div>
+  }
+
+  componentDidMount() {
+    this.setState({ month: new Date().getMonth() })
+
+    if (this.state.month > 2 && this.state.month < 9) {
+      this.setState({ season: this.props.lat > 0 ? 'summer' : 'winter' })
+    } else {
+      this.setState({ season: this.props.lat > 0 ? 'winter' : 'summer' })
+    }
+  }
 }
 
 export default SeasonDisplay;
