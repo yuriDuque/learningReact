4fd049187d6be05bbe637c0f35d5fe1a4a033ab9
YuriDuque <yurithielmann83@gmail.com>
2019-10-31 22:32:36 -0300

    lesson 77 - pics - showing froms to the user

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import SearchBar from './SeachBar'
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div><SearchBar /></div>
+  );
 }
 
 export default App;
