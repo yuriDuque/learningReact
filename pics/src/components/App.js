d4b891e9d307cf9c38f4a178705c04b76ab2c17b
YuriDuque <yurithielmann83@gmail.com>
2019-11-01 22:10:00 -0300

    lesson 90 - pics - viewing request results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 import SearchBar from './SeachBar';
 
+const api = 'https://api.unsplash.com/'
 class App extends React.Component {
   onSearchSubmit(term) {
+    axios.get(`${api}search/photos`, {
+      headers: {
+        Authorization: 'Client-ID b99d71b8036f689bdd4479cfb33d7a253379dd485a20b2ed7d2fc150e67c0c73'
+      },
 
+      params: {
+        query: term
+      }
+    })
   }
 
   render() {
