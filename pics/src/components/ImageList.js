b1519be205223ee73cf18eb00e1ae40357e4c10f
yuriDuque <yurithielmann83@gmail.com>
2019-11-02 00:49:21 -0200

    lesson 99 - pics - implementing keys in list

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    
+    const images = props.images.map(({description, id, urls}) => {
+        return (
+            <img key={id} alt={description} src={urls.regular}/>
+        );
+    })
 
     return(
-        <div></div>
+        <div>{images}</div>
     )
 }
 
