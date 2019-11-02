b4cc1b1919c44f235dfa65da4f59b6ac17521957
yuriDuque <yurithielmann83@gmail.com>
2019-11-02 00:29:48 -0200

    lesson 95 - pics - rendering lists

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import unsplash from '../api/unsplash'
 import SearchBar from './SeachBar';
+import ImageList from './ImageList'
 
 const api = 'https://api.unsplash.com/'
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
     const response = await unsplash.get('search/photos', {
       params: {query: term}
     }); 
-    
+
     this.setState({images: response.data.results})
   }
 
@@ -20,6 +21,7 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length}
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    
+
+    return(
+        <div></div>
+    )
+}
+
+export default ImageList;
