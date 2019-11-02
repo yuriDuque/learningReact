f1bf7389cf9fa2966d59b0b860befb225e6bb631
YuriDuque <yurithielmann83@gmail.com>
2019-11-01 22:00:06 -0300

    lesson 89 - pics - axios vs fetch

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-import SearchBar from './SeachBar'
+import SearchBar from './SeachBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+
   }
 
   render() {
