5a97718859f6b1c707acd2753e4042c21d4bd104
YuriDuque <yurithielmann83@gmail.com>
2019-11-04 21:13:02 -0300

    lesson 111 - videos - remider on event handlers

--- project_videos/src/components/SearchBar.js
+++ project_videos/src/components/SearchBar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
   render() {
     return (
-      <div>
-        SearchBar
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.term} />
+          </div>
+        </form>
       </div>
     );
   }
