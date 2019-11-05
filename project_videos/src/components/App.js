68564ba7400dc435c2ea38c94d76127cf4f3b88b
YuriDuque <yurithielmann83@gmail.com>
2019-11-04 22:19:52 -0300

    lesson 117 - videos - updating state with fetched data

--- project_videos/src/components/App.js
+++ project_videos/src/components/App.js
@@ -4,14 +4,26 @@ import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    console.log(term);
+  state = {
+    videos: []
+  }
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+    console.log(this.state.videos)
   }
 
   render() {
     return (
-      <div>
+      <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
