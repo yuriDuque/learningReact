277cb333e37019b6dc014fc38de4481d06a137b1
YuriDuque <yurithielmann83@gmail.com>
2019-11-04 22:47:58 -0300

    lesson 121 - videos - styling a list

--- project_videos/src/components/VideoItem.js
+++ project_videos/src/components/VideoItem.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
   // props.video
 
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title}
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+        {/* <div className="description">{video.snippet.description}</div> */}
+      </div>
     </div>
   );
 }
--- project_videos/src/components/VideoList.js
+++ project_videos/src/components/VideoList.js
@@ -9,7 +9,7 @@ const VideoList = ({ videos }) => {
   })
 
   return (
-    <div>{renderList}</div>
+    <div className="ui relaxed divided list">{renderList}</div>
   );
 }
 
