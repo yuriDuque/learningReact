5f7d01d7a647da3b5552612409b616c0f04632c2
YuriDuque <yurithielmann83@gmail.com>
2019-11-06 22:09:58 -0300

    lesson 147 - songs - the connect function

--- project_songs/src/components/App.js
+++ project_songs/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import selectSong from '../actions/index';
+import SongList from './SongList';
 
 const App = () => {
-
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
