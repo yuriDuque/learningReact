efee5aa7b7c0d31ad37e6aded82b65410c84e22e
YuriDuque <yurithielmann83@gmail.com>
2019-11-07 21:20:35 -0300

    lesson 153 - songs - conditional rendering

--- project_songs/src/components/SongDetail.js
+++ project_songs/src/components/SongDetail.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song!</div>
+  }
+
   return (
-    <div>Song Details</div>
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
   );
 };
 
 const mapStateToProp = state => {
-  return { song: state.selectedSong }
+  return { song: state.selectedSongReducer }
 }
 
 export default connect(mapStateToProp)(SongDetail);
